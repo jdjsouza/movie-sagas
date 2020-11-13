483746ca6dd69c27cbafac02dfcb82bb7ad278e6
xarsin <jdj.souza@gmail.com>
2020-11-13 06:52:01 -0600

    boiler plate pages created

---
+++ src/components/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class DetailsPage extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default DetailsPage;
