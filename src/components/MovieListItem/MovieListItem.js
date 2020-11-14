3d87d156c454f446584f765019a5b9c53aa60ea6
xarsin <jdj.souza@gmail.com>
2020-11-13 19:13:48 -0600

    added some styling so I can handle looking at the list

--- src/components/MovieListItem/MovieListItem.js
+++ src/components/MovieListItem/MovieListItem.js
@@ -2,19 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // To get the details page to work, we need withRouter so we can send the ID back
 import { withRouter } from 'react-router-dom';
+// gotta make it look good
+import './MovieListItem.css';
 
 class MovieListItem extends Component {
   render() {
     return (
       <div className="movieContainer">
         {/* go away */}
-        <div>
-          <img src={this.props.movie.poster} alt={this.props.movie.title} />
+        <div className="moviePoster">
+          <img
+            className="poster"
+            src={this.props.movie.poster}
+            alt={this.props.movie.title}
+          />
         </div>
-        <div>
+        <div className="movieInfo">
           <h3>{this.props.movie.title}</h3>
-        </div>
-        <div>
           <p>{this.props.movie.description}</p>
         </div>
         {/* // br */}
